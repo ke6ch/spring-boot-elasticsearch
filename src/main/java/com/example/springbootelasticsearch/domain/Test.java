dca2dee6269844f3b2aa5e80a8f6703f39adba9b
ke6ch <19749261+ke6ch@users.noreply.github.com>
2019-05-04 12:40:49 +0900

    Add Jackson

---
+++ src/main/java/com/example/springbootelasticsearch/domain/Test.java
@@ -0,0 +1,8 @@
+package com.example.springbootelasticsearch.domain;
+
+import lombok.Data;
+
+@Data
+public class Test {
+	private String name;
+}
--- src/main/java/com/example/springbootelasticsearch/service/ElasticsearchService.java
+++ src/main/java/com/example/springbootelasticsearch/service/ElasticsearchService.java
@@ -1,6 +1,8 @@
 package com.example.springbootelasticsearch.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.search.SearchRequest;
@@ -11,22 +13,26 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
+
+import com.example.springbootelasticsearch.domain.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class ElasticsearchService {
 
-    public String search() {
+    public List<Test> search() {
 
     	RestHighLevelClient client = new RestHighLevelClient(
     	        RestClient.builder(
     	                new HttpHost("localhost", 9200, "http"),
     	                new HttpHost("localhost", 9201, "http"),
     	                new HttpHost("localhost", 9202, "http")));
+
+    	List<Test> result = new ArrayList<>();
     	
-    	SearchRequest searchRequest = new SearchRequest(); 
+    	SearchRequest searchRequest = new SearchRequest("library"); 
     	SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder(); 
     	searchSourceBuilder.query(QueryBuilders.matchAllQuery()); 
     	searchRequest.source(searchSourceBuilder); 
@@ -34,20 +40,18 @@ public class ElasticsearchService {
         try {
         	SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
         	RestStatus status = searchResponse.status();
-        	SearchHits searchHits = searchResponse.getHits();
         	
-        	System.out.println(status);
-        	System.out.println(searchHits);
-        	System.out.println("AAA");
-
-            for (SearchHit hit : searchHits) {
-            	System.out.println(hit.getSourceAsString());
-            }
+        	ObjectMapper mapper = new ObjectMapper();
         	
+        	for (SearchHit hit : searchResponse.getHits()) {
+        		Test test = mapper.readValue(hit.getSourceAsString(), Test.class);
+        		result.add(test);
+        	}
+        	        	
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        
-    	return "AVC";
+        }       
+    	return result;
+
     }
 }
