f7a0a19bc875efe2427d84908d996cf38eb3d3c5
k <devs079@yahoo.co.jp>
2019-05-04 12:40:49 +0900

    Add Jackson

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SpringBoot, Elasticsearch
+
+```
+java -jar ./target/spring-boot-elasticsearch-0.0.1-SNAPSHOT.jar
+curl localhost:8080/
+```
--- src/main/java/com/example/springbootelasticsearch/controller/ElasticsearchController.java
+++ src/main/java/com/example/springbootelasticsearch/controller/ElasticsearchController.java
@@ -1,14 +1,13 @@
 package com.example.springbootelasticsearch.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.springbootelasticsearch.domain.Test;
 import com.example.springbootelasticsearch.service.ElasticsearchService;
 
 @Controller
@@ -17,14 +16,10 @@ public class ElasticsearchController {
     @Autowired
     ElasticsearchService elasticsearchService;
 
-    @RequestMapping("/user")
-    public 	List<String> users(Model model) {
-        model.addAttribute("users", elasticsearchService.search());
-
-        List<String> list = new ArrayList<String>();
-		list.add("太宰");
-		list.add("夏目");
-		return list;
+    @RequestMapping("/")
+    @ResponseBody
+    public List<Test> users() {
+    	return elasticsearchService.search();
     }
 
 }
