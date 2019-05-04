918d50596ff0f4302304dc88a01b020f7ad45642
ke6ch <19749261+ke6ch@users.noreply.github.com>
2019-05-04 15:08:49 +0900

    Add Config

--- src/main/java/com/example/springbootelasticsearch/service/ElasticsearchService.java
+++ src/main/java/com/example/springbootelasticsearch/service/ElasticsearchService.java
@@ -14,6 +14,7 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.springbootelasticsearch.domain.Test;
@@ -22,14 +23,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class ElasticsearchService {
 
+	@Autowired
+    private RestHighLevelClient client;
+	
     public List<Test> search() {
-
-    	RestHighLevelClient client = new RestHighLevelClient(
-    	        RestClient.builder(
-    	                new HttpHost("localhost", 9200, "http"),
-    	                new HttpHost("localhost", 9201, "http"),
-    	                new HttpHost("localhost", 9202, "http")));
-
     	List<Test> result = new ArrayList<>();
     	
     	SearchRequest searchRequest = new SearchRequest("library"); 
@@ -39,19 +36,15 @@ public class ElasticsearchService {
 
         try {
         	SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-        	RestStatus status = searchResponse.status();
-        	
+
         	ObjectMapper mapper = new ObjectMapper();
-        	
         	for (SearchHit hit : searchResponse.getHits()) {
         		Test test = mapper.readValue(hit.getSourceAsString(), Test.class);
         		result.add(test);
         	}
-        	        	
         } catch (IOException e) {
             e.printStackTrace();
         }       
     	return result;
-
     }
 }
